6fb7afdc5d4ce0147d97b146d748327ea5d5a3fa
thanhtinhpas1 <thanhtinhpas1@gmail.com>
2020-02-07 19:20:54 +0700

    fix communication

--- scripts/up.sh
+++ scripts/up.sh
@@ -6,4 +6,6 @@ export CONTAINER_COMMAND="yarn start:dev"
 export CONTAINER_SCALE="1"
 export CONTAINER_PORT="7070"
 
+# remove cache volume cause mysql communication failed with root user
+docker-compose rm -fv
 docker-compose up -d --build
