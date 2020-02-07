2a1cafe6c4eb3eb7d2d8212d51a69dd60f2e8696
tinh <thanhtinhpas1@gmail.com>
2020-02-07 13:20:09 +0700

    fix docker compose

--- src/users/events/impl/user-deleted.event.ts
+++ src/users/events/impl/user-deleted.event.ts
@@ -2,5 +2,5 @@ import { IEvent } from '@nestjs/cqrs';
 
 export class UserDeletedEvent implements IEvent {
   constructor(
-    public readonly userId: string) {}
+    public readonly userId: number) {}
 }
