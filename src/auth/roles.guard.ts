8fe051a3eda43bbc9e9a205ee4819b9673b57691
Tinh Tran <tinhtran@kms.com.vn>
2020-02-22 11:28:42 +0700

    udpate role & env

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,17 +1,19 @@
-import { Injectable, CanActivate, Inject } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { JwtService } from "@nestjs/jwt";
-import { jwtConstants } from "auth/constants";
-import { UsersService } from "users/services/users.service";
+import {CanActivate, Injectable} from '@nestjs/common';
+import {Reflector} from '@nestjs/core';
+import {JwtService} from '@nestjs/jwt';
+import {UsersService} from '../users/services/users.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
 
-    constructor(private readonly reflector: Reflector,
+    constructor(
+        private readonly reflector: Reflector,
         private readonly jwtService: JwtService,
-    ) { }
+        private readonly  usersService: UsersService,
+    ) {
+    }
 
-    canActivate(context: import("@nestjs/common").ExecutionContext): boolean | Promise<boolean> | import("rxjs").Observable<boolean> {
+    canActivate(context: import('@nestjs/common').ExecutionContext): boolean | Promise<boolean> | import('rxjs').Observable<boolean> {
         const roles = this.reflector.get<string[]>('roles', context.getHandler());
         const request = context.switchToHttp().getRequest();
         if (!roles) {
@@ -24,9 +26,8 @@ export class RolesGuard implements CanActivate {
     matchRoles(request, roles: string[]) {
         var authorization = request.headers.authorization;
         if (!authorization) return false;
-        const jwt = authorization.replace("Bearer ", "");
-        var payload = this.jwtService.decode(jwtConstants.secret);
-        console.log(payload);
+        const jwt = authorization.replace('Bearer ', '');
+        const payload = this.jwtService.decode(jwt);
         // load roles
         // var user = this.usersService.findUserRoles(payload.userId);
         return false;
---
+++ src/common/constant.ts
@@ -0,0 +1,4 @@
+export const ROLE_ADMIN = 'ADMIN';
+export const ROLE_USER = 'USER';
+export const ROLE_MANAGER_USER = 'MANAGER_USER';
+export const BEARER_HEADER_AUTHORIZE = 'Bearer  ;'
