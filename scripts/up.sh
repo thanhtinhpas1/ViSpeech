5e4000d2f624f2d035dffa44b4ad92adb98276ec
Tinh Tran <tinhtran@kms.com.vn>
2020-02-27 00:34:28 +0700

    add release

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,6 +5,10 @@ export DIR_DATA_PATH="$PWD"
 export CONTAINER_COMMAND="yarn start:dev"
 export CONTAINER_SCALE="1"
 export CONTAINER_PORT="7070"
+# release app and vispeechdemo
+export CONTAINER_NODE_COMMAND="yarn start"
+export VISPEECH_PORT_DEMO=3000
+export RELEASE_PAGE_PORT=80
 
 # remove cache volume cause mysql communication failed with root user
 # docker-compose rm -fv
@@ -13,7 +17,4 @@ docker-compose up -d --build
 #vispeech app
 export VISPEECH_PORT=7070
 
-# release app and vispeechdemo
-export CONTAINER_NODE_COMMAND="yarn start"
-export VISPEECH_PORT_DEMO=3000
-export RELEASE_PAGE_PORT=80
+
