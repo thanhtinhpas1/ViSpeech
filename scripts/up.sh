5ef447ec8b9b157f60d2f3d4cdfcb6887f9b9559
Tinh Tran <tinhtran@kms.com.vn>
2020-02-27 01:23:45 +0700

    add release

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,7 +2,7 @@
 
 export DIR_DATA_PATH="$PWD"
 
-export CONTAINER_COMMAND="yarn start:dev"
+export CONTAINER_COMMAND="yarn start"
 export CONTAINER_SCALE="1"
 export CONTAINER_PORT="7070"
 # release app and vispeechdemo
