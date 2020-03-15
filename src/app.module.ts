d2c8bb8db5ae69e265304b9e65fc739457619126
thanhtinhpas1 <thanhtinhpas1@gmail.com>
2020-03-16 02:13:40 +0700

    fix conflict & add _id

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,24 @@
-import { Module } from "@nestjs/common";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AuthModule } from "auth/auth.module";
-import { OrdersModule } from "orders/orders.module";
-import { join } from "path";
-import { ReportsModule } from "reports/reports.module";
-import { RolesModule } from "roles/roles.module";
-import { TokensModule } from "tokens/tokens.module";
-import { UsersModule } from "users/users.module";
-import { config } from "../config";
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'auth/auth.module';
+import { OrdersModule } from 'orders/orders.module';
+import { join } from 'path';
+import { ReportsModule } from 'reports/reports.module';
+import { RolesModule } from 'roles/roles.module';
+import { TokensModule } from 'tokens/tokens.module';
+import { UsersModule } from 'users/users.module';
+import { config } from '../config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...config.DATABASE,
       useUnifiedTopology: true,
-      entities: [__dirname + "/../**/*.dto{.ts,.js}"],
+      entities: [__dirname + '/../**/*.dto{.ts,.js}'],
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", config.ROOT_PATH)
+      rootPath: join(__dirname, '..', config.ROOT_PATH),
     }),
     /** ------------- */
     UsersModule,
