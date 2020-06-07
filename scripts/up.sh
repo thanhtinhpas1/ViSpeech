57704a4ad78de02346024b97b7d89caad2906569
Tinh Tran <tinhtran@kms-technology.com>
2020-06-07 21:55:48 +0700

    remove api key for stripe and sendgrid

--- scripts/up.sh
+++ scripts/up.sh
@@ -11,8 +11,6 @@ export VISPEECH_PORT_DEMO=5000
 export VISPEECH_PORT=7070
 export RELEASE_INTERNAL_PORT=8000
 export RELEASE_PAGE_PORT=80
-export SENDGRID_API_KEY='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'
-export STRIPE_SECRET_KEY='sk_test_U1j7h9gyeVW2EP4bWwPlunzW00zQinDEbi'
 
 #  docker-compose rm --all &&
 #  docker-compose pull &&
