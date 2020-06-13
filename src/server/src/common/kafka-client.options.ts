fd7a024c580830850dd5699bd4e199cc5b505680
Tinh Tran <tinhtran@kms-technology.com>
2020-06-13 15:56:26 +0700

    Update kafka timeout and EventStore reconnect when break connection.

--- src/server/src/common/kafka-client.options.ts
+++ src/server/src/common/kafka-client.options.ts
@@ -7,10 +7,14 @@ export const kafkaClientOptions: KafkaOptions = {
         client: {
             clientId: 'vispeech',
             brokers: [`${config.KAFKA.HOST}:${config.KAFKA.PORT}`],
+            authenticationTimeout: 10000,
+            requestTimeout: 10000,
+            connectionTimeout: 30000,
         },
         producer: {
             transactionalId: config.KAFKA.NAME,
             allowAutoTopicCreation: true,
+            transactionTimeout: 30000,
         },
     },
 }
